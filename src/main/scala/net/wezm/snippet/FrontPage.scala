220ed4dad47929e6faab2557cc00b568cdb3812f
Wesley Moore <wez@wezm.net>
2009-10-01 19:40:04 +1000

    Get some basic bindings working on the front page

--- src/main/scala/net/wezm/snippet/FrontPage.scala
+++ src/main/scala/net/wezm/snippet/FrontPage.scala
@@ -1,50 +1,29 @@
 package net.wezm.snippet
 
-import net.wezm._
-import model._
-
-import net.liftweb._
-import http._
-import SHtml._
+import scala.xml._
+import net.liftweb.http._
+import net.liftweb.util._
 import S._
-
-import js._
-import JsCmds._
-
-import mapper._
-
-import util._
+import SHtml._
 import Helpers._
+import scala.xml._
 
-import scala.xml.{NodeSeq, Text}
+import net.wezm.model._
+
+import java.util.Date
 
 class FrontPage {
+  val formatter = new java.text.SimpleDateFormat("yyyy/MM/dd")
 
-  private def toShow = Page.findAll()
-
-  private def doList(html: NodeSeq): NodeSeq = 
-   toShow. 
-   flatMap(page => 
-     bind("page", html,
-        "date" -> "1 October, 2009",
-        "category" -> "Category",
-        "commentCount" -> "No comments"
-     ))
-
-  def list(html: NodeSeq) = {
-    //val id = S.attr("all_id").open_! 
-
-    // def inner(): NodeSeq = { 
-    //   //def reDraw() = SetHtml(id, inner()) 
-    // 
-    //   bind("todo", html, 
-    //      "list" -> doList(html)) 
-    // } 
-    // 
-    // inner() 
-    bind("page", html, 
-       "list" -> doList(html)) 
-    
+  def summary (xhtml : NodeSeq) : NodeSeq = {
+    val entries : NodeSeq = Page.findAll match {
+    	case Nil => Text("You have no pages, go write something") // Add link to create one...
+    	case pages => pages.flatMap({page => 
+    	  bind("page", chooseTemplate("book", "entry", xhtml),
+    	       "title" -> Text(page.title.toString),
+    	       "category" -> Text("Bound Category"))
+      })
+    }
+    bind("book", xhtml, "entry" -> entries)
   }
-
 }
