84243c618503b8507e5dfdea1bf86100db2b9691
Wesley Moore <wez@wezm.net>
2009-10-06 13:41:10 +1100

    Attempt to load and display article take 2
    Still doesn't work

--- src/main/scala/net/wezm/snippet/ArticlePage.scala
+++ src/main/scala/net/wezm/snippet/ArticlePage.scala
@@ -7,6 +7,7 @@ import Helpers._
 import _root_.scala.xml._
 
 import java.util.Date
+import net.liftweb.textile._
 
 import net.wezm.model._
 
@@ -14,12 +15,15 @@ class ArticlePage {
   // val formatter = new java.text.SimpleDateFormat("yyyy/MM/dd")
 
   def show (xhtml : NodeSeq) : NodeSeq = {
-    val article: NodeSeq = Article.find(1l) match { // XXX: Hardcoded id, should find from param By(Article.id, 1l)
-    	case Empty => Text("Unable to find article")
-    	case Full(article: Article) => (
-    	  "title" -> Text(article.title),
-    	  "body" -> Text(article.body)  // Parse Textile
-    )}
-    bind("article", xhtml, article)
+    //val article = Article.find(By(Article.slug, S.param("permalink"))) match { // XXX: Hardcoded id, should find from param By(Article.id, 1l)
+    val article = Article.find(1l) match {
+    	case Full(article: Article) => article
+    	case _ => return NotFoundResponse()
+    }
+    
+    bind("article", xhtml,
+      "title" -> article.title,
+      "body" -> TextileParser.toHtml(article.body)
+    )
   }
 }
